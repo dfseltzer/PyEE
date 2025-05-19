removed array stuff for units as its was half implemented.  some tests are broke - fix them.  started adding smps classes

pyee/types/units.py
@@ -57,10 +57,8 @@ class Prefix(object):
 
     Supports multiplication and division.  If used against another Prefix instance, returns a
     new Prefix instance.  Otherwise acts like a number and attempts the operation.
-
-
-    To generate an array, use the from_number helper.
     """
+
     _DATA_FILE = "SI_prefixes"
     _data_by_symbol = None
     _data_by_name = None
@@ -69,8 +67,8 @@ class Prefix(object):
 
     __DEBUG = False
 
-    @classmethod
-    def from_name(cls, pstring):
+    @staticmethod
+    def from_name(pstring):
         """
         Searches for a prefix matching the given symbol.  Does not check for multiple matches... as
         this should never happen.  If no match is made, raises key error.
@@ -82,19 +80,16 @@ class Prefix(object):
         """
         pobj = Prefix() # forces load of initial dict... dumb but simple
 
-        if cls._data_by_name is None:
-            cls._data_by_name = {d["name"]: {"symbol":s, "factor":d["factor"]} for s, d in cls._data_by_symbol.items()}
+        pdata = Prefix._data_by_name[pstring] # type: ignore
 
-        pdata = cls._data_by_name[pstring]
-
-        pobj.n = [pstring]
-        pobj.f = [pdata["factor"]]
-        pobj.s = [pdata["symbol"]]
+        pobj.n = pstring
+        pobj.f = pdata["factor"]
+        pobj.s = pdata["symbol"]
 
         return pobj
 
-    @classmethod
-    def from_number(cls, num):
+    @staticmethod
+    def from_number(num):
         """
         Returns the closest prefix to use for the given number.  Attempts to use a prefix that
         will result in a number formatted "well", meaning 1-3 digits ahead of the decimal place.
@@ -105,49 +100,33 @@ class Prefix(object):
         #TODO array-itize this... some messy lists mixed with arrays below.
 
         pobj = Prefix()  # forces load of initial dict... dumb but simple
-        if cls._data_by_value is None:
-            cls._data_by_value = {d["factor"]: {"symbol":s, "name":d["name"]} for s, d in cls._data_by_symbol.items()}
-            cls._data_value_scale = sorted(cls._data_by_value.keys())
 
-        #make an array so its always consistent...
-        try:
-            nvals = len(num)
-        except TypeError:
-            num = np.array([num])
-            nvals = 1 
         pnum = abs(num)
 
         # if greater than 1, we want the next smallest factor.
         #scalar was: diffs = [1 if (pnum - v) >= 0 else 0 for v in cls._data_value_scale]
-        diffs = np.array([1*((pnum - v) >= 0) for v in cls._data_value_scale])
-
-        factornum = [cls._data_value_scale[max(np.max(diffs[:,idx].nonzero()),0)] for idx in range(nvals)]
-
-        if len(factornum) == 1:
-            factornum = factornum[0]
-            pobj.s = cls._data_by_value[factornum]["symbol"]
-            pobj.n = cls._data_by_value[factornum]["name"]
-        else:
-            factornum = np.array(factornum)
-            pobj.s = [cls._data_by_value[fn]["symbol"] for fn in factornum]
-            pobj.n = [cls._data_by_value[fn]["name"] for fn in factornum]
-
-        pobj.f = factornum
+        diffs = np.array([1*((pnum - v) >= 0) for v in Prefix._data_value_scale]) # type: ignore
+        pobj.f = Prefix._data_value_scale[max(np.max(diffs.nonzero()),0)] # type: ignore
+        pobj.s = Prefix._data_by_value[pobj.f]["symbol"] # type: ignore
+        pobj.n = Prefix._data_by_value[pobj.f]["name"] # type: ignore
         return pobj
 
     def __init__(self, symbol=""):
-        if self._data_by_symbol is None:
+        if Prefix._data_by_symbol is None:
             Prefix._data_by_symbol = load_data_file(Prefix._DATA_FILE)
+            Prefix._data_by_value = {d["factor"]: {"symbol":s, "name":d["name"]} for s, d in Prefix._data_by_symbol.items()}
+            Prefix._data_value_scale = sorted(Prefix._data_by_value.keys())
+            Prefix._data_by_name = {d["name"]: {"symbol":s, "factor":d["factor"]} for s, d in Prefix._data_by_symbol.items()}
 
         self.f = 1
         self.n = ""
-        self.s = [symbol]
+        self.s = symbol
 
         if self.s is None:
             return
 
-        self.f = [self._data_by_symbol[symbol]["factor"]]
-        self.n = [self._data_by_symbol[symbol]["name"]]
+        self.f = self._data_by_symbol[symbol]["factor"]  # type: ignore
+        self.n = self._data_by_symbol[symbol]["name"]  # type: ignore
 
     def __copy__(self):
         return copy.deepcopy(self)
@@ -162,7 +141,7 @@ class Prefix(object):
         try:
             nvals = len(self.f)
         except TypeError:
-            return self.s
+            return self.s[0]
         
         if nvals < 5:
             return "["+", ".join([f"{v}" for v in self.f])+"]"